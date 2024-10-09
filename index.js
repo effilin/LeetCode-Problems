d8498b734f5791df6b12a48b63a115a2a50affcd
effilin <effienummer@gmail.com>
2024-10-09 14:11:02 -0400

    leekproblem code update

--- index.js
+++ index.js
@@ -30,4 +30,42 @@ var mergeAlternately = function(word1, word2) {
     return zippered
 };
 
-mergeAlternately( 'abc', 'qrs');
+// mergeAlternately( 'abc', 'qrs');
+
+/* problem #6 https://leetcode.com/problems/zigzag-conversion/
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if (numRows === 1 || numRows >= s.length) {
+        return s
+    };
+    const rows = new Array(numRows).fill().map(() => []);
+    let dir = 1;
+    let rowNumber = 0;
+
+    
+    for (i=0; i <= s.length; i++) {
+        let myLetter = s.charAt(i);
+        rows[rowNumber].push(myLetter)
+
+        if (dir === 1) {
+        rowNumber ++
+            if ( (rowNumber - 1) === numRows) {
+                dir = -1;
+            }
+        }
+        if (dir === -1) {
+            rowNumber -= 1;
+            if (rowNumber === 0) {
+                dir = 1;
+            }
+        }
+    }
+        console.log(rows)
+
+    };
+
+    
+convert("PAYPALISHIRING", 3);
